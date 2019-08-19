d42c291f956a2d53070f810fad523e83c0a045ca
Owen Thomas <Owen.Thomas@cardiff.gov.uk>
2019-08-19 13:56:21 +0100

    step 8 (Comonents)

--- ACM/Product.cs
+++ ACM/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace ACM
+﻿using Acme.Common;
+
+namespace ACM
 {
     public class Product : EnitityBase
     {
@@ -12,7 +14,21 @@
             ProductId = productId;
         }
         public int ProductId { get; private set; }
-        public string ProductName { get; set; }
+
+        private string _productName;
+        public string ProductName
+        {
+            get
+            {
+                return _productName.InsertSpaces();
+            }
+            set
+            {
+                _productName = value;
+            }
+        }
+
+
         public string ProductDescription { get; set; }
         public decimal? CurrentPrice { get; set; }
 
